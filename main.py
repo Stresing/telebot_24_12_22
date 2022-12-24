cf7ef4d13bdbe95ec074caa4b5f4890673adbef4
stresing <Stresingrad@gmail.com>
2022-12-24 10:21:01 +0700

    final task complit

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ def start(msg):
     markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
     key_zodiac_signs = telebot.types.KeyboardButton('Знак Зодиака')
     key_guess_number = telebot.types.KeyboardButton('Угадай число')
-    markup.add(key_zodiac_signs, key_guess_number)
+    key_stickers = telebot.types.KeyboardButton('Узнать ID стикера')
+    markup.add(key_zodiac_signs, key_guess_number, key_stickers)
     bot.send_message(msg.chat.id, 'Выбирай', reply_markup=markup)
 
 
@@ -35,35 +36,69 @@ def answer(msg):
     elif msg.text == 'Угадай число':
         num = bot.send_message(msg.chat.id, 'Я загадал число от 1-3, отгадай его')
         bot.register_next_step_handler(num, num_random)
+    elif msg.text == 'Узнать ID стикера':
+        msg = bot.send_message(msg.chat.id, 'Отправь сюда стикер ID которого хочешь узнать')
+        bot.register_next_step_handler(msg, sticker_find_id)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
     zodiac = find_zodiac()
+    stick_zodiac = find_stickers_zodiac()
     if call.data == '1':
-        bot.send_message(call.message.chat.id, zodiac['1'])
+        bot.send_sticker(call.message.chat.id, stick_zodiac[
+            '1'].rstrip())  # str.rstrip() обрезает все ненужные знаки типа \n и символы chars -->
+        bot.send_message(call.message.chat.id, zodiac['1'])  # без этого метода выдаёт ошибку о длинне строки
+
     elif call.data == '2':
+        bot.send_sticker(call.message.chat.id, stick_zodiac['2'].rstrip())
         bot.send_message(call.message.chat.id, zodiac['2'])
+
     elif call.data == '3':
+        bot.send_sticker(call.message.chat.id, stick_zodiac['3'].rstrip())
         bot.send_message(call.message.chat.id, zodiac['3'])
+
     elif call.data == '4':
+        bot.send_sticker(call.message.chat.id, stick_zodiac['4'].rstrip())
         bot.send_message(call.message.chat.id, zodiac['4'])
+
     elif call.data == '5':
+        bot.send_sticker(call.message.chat.id, stick_zodiac['5'].rstrip())
         bot.send_message(call.message.chat.id, zodiac['5'])
+
     elif call.data == '6':
+        bot.send_sticker(call.message.chat.id, stick_zodiac['6'].rstrip())
         bot.send_message(call.message.chat.id, zodiac['6'])
+
     elif call.data == '7':
+        bot.send_sticker(call.message.chat.id, stick_zodiac['7'].rstrip())
         bot.send_message(call.message.chat.id, zodiac['7'])
+
     elif call.data == '8':
+        bot.send_sticker(call.message.chat.id, stick_zodiac['8'].rstrip())
         bot.send_message(call.message.chat.id, zodiac['8'])
+
     elif call.data == '9':
+        bot.send_sticker(call.message.chat.id, stick_zodiac['9'].rstrip())
         bot.send_message(call.message.chat.id, zodiac['9'])
+
     elif call.data == '10':
+        bot.send_sticker(call.message.chat.id, stick_zodiac['10'].rstrip())
         bot.send_message(call.message.chat.id, zodiac['10'])
+
     elif call.data == '11':
+        bot.send_sticker(call.message.chat.id, stick_zodiac['11'].rstrip())
         bot.send_message(call.message.chat.id, zodiac['11'])
+
     elif call.data == '12':
+        bot.send_sticker(call.message.chat.id, stick_zodiac['12'].rstrip())
         bot.send_message(call.message.chat.id, zodiac['12'])
+
+
+@bot.message_handler(content_types=["sticker"])
+def sticker_find_id(message):
+    sticker_id = message.sticker.file_id
+    bot.send_message(message.chat.id, f'Вот ID твоего стикера: \n {sticker_id}')
 
 
 def find_zodiac():
@@ -74,15 +109,22 @@ def find_zodiac():
             dict[str1[0]] = str1[1]
         return dict
 
+
+def find_stickers_zodiac():
+    with open('zodiac_stickers.txt', 'r', encoding='utf-8') as file:
+        dict = {}
+        for i in range(1, 13):
+            str1 = file.readline().split(' ', 1)
+            dict[str1[0]] = str1[1]
+        return dict
+
+
 def num_random(message):
     x = random.randint(0, 3)
     if message.text == str(x):
         bot.send_message(message.chat.id, f'Угадал, моё число было {x}, Ура!')
     else:
         bot.send_message(message.chat.id, f'Ты не угадал, мой число было {x}...\n Попробуй ещё раз!')
-
-
-
 
 
 bot.polling(none_stop=True)
